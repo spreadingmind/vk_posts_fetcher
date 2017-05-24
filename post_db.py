467c8defc69a5d0b79535e231feeebddde2733bc
spreadingmind <lisabeth.shevtsova@gmail.com>
2017-05-24 22:23:52 +0300

    update db instead oof insert

--- post_db.py
+++ post_db.py
@@ -6,15 +6,12 @@ db = client.my_vk_posts
 # posts = main()
 
 def insert_into_db(posts):
-    if posts:
-        result = db.my_vk_posts.insert_many([{
+    for post in posts:
+        db.my_vk_posts.update_one({'id': post.id}, {'$set': {
         'id': post.id,
         'date': str(post.date),
         'url' : post.url,
         'text': post.text,
         'likes': post.likes,
-        'reposts': post.reposts} for post in posts
-            if post and db.my_vk_posts.find({'id': post.id}).count() == 0])
-        return result
-    else:
-        return 'Nothing to add to DB'
+        'reposts': post.reposts}}, upsert=True)
+
